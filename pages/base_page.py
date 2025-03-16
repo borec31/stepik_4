c2c2ae34f97c9a459bc1e58c822f1b4c363469e3
borec31 <154601539+borec31@users.noreply.github.com>
2025-03-16 13:20:19 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,27 +1,32 @@
-from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
 
 class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-
+    
     def open(self):
-        """Открытие страницы"""
         self.browser.get(self.url)
-
+    
     def go_to_basket(self):
-        """Переход в корзину по кнопке в шапке"""
-        basket_link = self.browser.find_element(By.CSS_SELECTOR, ".basket-mini a.btn")
-        basket_link.click()
-
-
-
-# class BasePage():
-#     def go_to_login_page(self):
-#         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
-#         link.click()
-#
-#     def should_be_login_link(self):
-#         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
-
+        self.browser.find_element(*BasePageLocators.BASKET_LINK).click()
+    
+    def go_to_login_page(self):
+        self.browser.find_element(*BasePageLocators.LOGIN_LINK).click()
+    
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+    
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
