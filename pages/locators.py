23ab0a8988a8d75a88a0d6ec9aa616a86ac3ee65
borec31 <154601539+borec31@users.noreply.github.com>
2025-03-16 13:19:53 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,16 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators():
+class BasePageLocators:
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class BasePageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+class ProductPageLocators:
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "#add_to_basket_form button[type='submit']")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success .alertinner")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
 
+class BasketPageLocators:
+    BASKET_ITEMS = (By.CSS_SELECTOR, "#basket_formset")
+    EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
