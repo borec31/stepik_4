873269ca5edd1b7150736b50f4a19319ffc5a472
borec31 <154601539+borec31@users.noreply.github.com>
2025-03-16 13:53:07 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,11 +1,22 @@
 import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        action="store",
+        default="en",
+        help="Choose language: en, es, fr, etc."
+    )
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "need_review: mark tests that need peer review")
 
-@pytest.fixture(scope="session")
-def browser():
-    from selenium import webdriver
-    driver = webdriver.Chrome()  # Предполагается, что ChromeDriver установлен
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("--language")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("prefs", {"intl.accept_languages": language})
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
