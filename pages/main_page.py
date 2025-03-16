500bef5aaade3497f2f6d694ddc06813860f8368
borec31 <154601539+borec31@users.noreply.github.com>
2025-03-16 13:22:24 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,5 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
-from .locators import MainPageLocators
-
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+    def __init__(self, browser):
+        super().__init__(browser, "http://selenium1py.pythonanywhere.com/")
